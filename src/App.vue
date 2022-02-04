b1608f4e0ca123cc7f4959f41636a041304b8d30
mako4kamiya <mako4kamiya@gmail.com>
2022-02-04 11:56:56 +0900

    アクセス制御

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
-  <amplify-greetings></amplify-greetings>
+  <div v-if="isSignedIn">
+    <amplify-greetings></amplify-greetings>
+  </div>
   <router-view />
 </template>
+
+<script>
+export default {
+  computed: {
+    isSignedIn() {
+      return this.$store.state.user !== null;
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    user: null,
+  },
+  mutations: {
+    setUser(state, user){
+      state.user = user;
+    }
+  },
   actions: {},
   modules: {},
 });
